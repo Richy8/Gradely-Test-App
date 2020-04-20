3d940b3830381c67be4afa891cb7248d4a013fc6
richy8 <menaelvisjones@gmail.com>
2020-04-20 23:58:36 +0100

    Second Commit - SignUp page

---
+++ src/components/signup/SignUpForm.vue
@@ -0,0 +1,138 @@
+<template>
+  <div class="section-three">
+    <form @click.prevent>
+      <!-- INPUT FORM FOR FIRST AND LAST NAME -->
+      <div class="form-group fullname">
+        <!-- FIRSTNAME -->
+        <div class="firstname">
+          <label for="firtsName">FIRST NAME</label>
+          <input
+            type="text"
+            v-model="form.firstname"
+            class="form-control font-13 title-black"
+            id="firstName"
+            placeholder="E.g David"
+          >
+        </div>
+        <!-- LASTNAME -->
+        <div class="lastname">
+          <label for="lastName">LAST NAME (SURNAME)</label>
+          <input
+            type="text"
+            v-model="form.lastname"
+            class="form-control font-13 title-black"
+            id="lastName"
+            placeholder="E.g Nwankwo"
+          >
+        </div>
+      </div>
+
+      <!-- NAME OF SCHOOL -->
+      <div class="form-group">
+        <label for="schoolName">SCHOOL NAME</label>
+        <input
+          type="text"
+          v-model="form.school"
+          class="form-control font-13 title-black"
+          id="schoolName"
+          placeholder="The Name of your School"
+        >
+      </div>
+
+      <!-- EMAIL ADDRESS -->
+      <div class="form-group">
+        <label for="emailAddress">EMAIL ADDRESS</label>
+        <input
+          type="email"
+          v-model="form.email"
+          class="form-control font-13 title-black"
+          id="emailAddress"
+          placeholder="E.g davidisaboy@gmail.com"
+        >
+      </div>
+
+      <!-- PHONE NUMBER -->
+      <div class="form-group">
+        <label for="phoneNumber">PHONE NUMBER</label>
+        <div class="input-group">
+          <div class="input-group-prepend">
+            <span class="input-group-text font-13 title-black">+ 234</span>
+          </div>
+          <input
+            type="number"
+            v-model="form.phone"
+            class="form-control font-13 title-black"
+            id="phoneNumber"
+            placeholder="Enter phone number"
+          >
+        </div>
+      </div>
+
+      <!-- PASSWORD -->
+      <div class="form-group">
+        <label for="password">PASSWORD</label>
+        <div class="input-group">
+          <input
+            :type="passwordType"
+            v-model="form.password"
+            class="form-control font-13 title-black"
+            id="password"
+            placeholder="Create a Password"
+          >
+          <div class="input-group-append">
+            <span class="input-group-text font-13 title-black pointer">
+              <b-icon icon="eye" class="title-black" @click="viewPassword"></b-icon>
+            </span>
+          </div>
+        </div>
+      </div>
+
+      <!-- SUBMIT BUTTON -->
+      <div class="form-group mx-0 px-0 d-flex justify-content-start align-items-center">
+        <button
+          type="submit"
+          class="btn btn-md navy-blue orange-light-bg mx-0 rounded-25 font-weight-bold"
+        >CREATE ACCOUNT</button>
+
+        <div class="policy-text w-50 font-12 ml-4 font-weight-400 dark-ash">
+          By creating an account you agree to our
+          <span>
+            <a href="#" class="orange-light">Terms</a>
+          </span> and
+          <span>
+            <a href="#" class="orange-light">Policies</a>
+          </span>
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SignUpForm",
+  data() {
+    return {
+      form: {
+        firstname: "",
+        lastname: "",
+        school: "",
+        email: "",
+        phone: "",
+        password: ""
+      },
+      passwordType: "password"
+    };
+  },
+  methods: {
+    // METHOD TO VIEW PASSWORD
+    viewPassword() {
+      this.passwordType =
+        this.passwordType === "password" ? "text" : "password";
+    }
+  }
+};
+</script>
+
+<style lang='scss'>
+</style>
--- src/pages/school/signup/Signup.vue
+++ src/pages/school/signup/Signup.vue
@@ -1,16 +1,85 @@
 <template>
-    <section class="signup-section">
+  <!-- SIGNUP PAGE  -->
+  <section class="signup-section">
+    <b-container fluid>
+      <b-row class="container-row">
+        <!-- LEFT PANE -->
+        <b-col md="5" class="left-pane d-none d-md-block p-0 z-depth-3">
+          <img
+            src="https://res.cloudinary.com/richy-jones/image/upload/v1587381343/GRADELY_ASSETS/SignupBg.png"
+            alt="LeftPaneImg"
+            class="left-pane-content pane-img"
+          >
+          <!-- IMG OVERLAY -->
+          <div class="left-pane-content pane-img-overlay"></div>
+          <!-- LEFT PANE CONTENT -->
+          <div
+            class="content-box px-md-4 p-lg-5 d-flex flex-column justify-content-center align-items-center"
+          >
+            <div class="content">
+              <span class="section-one">Gradely for schools</span>
+              <h3 class="section-two">Come a step closer to the future of Education</h3>
+              <ul class="section-three list-unstyled">
+                <li>
+                  <b-icon icon="dash"></b-icon>Experience the power of Digitised homework
+                </li>
+                <li>
+                  <b-icon icon="dash"></b-icon>Track your school and class activities with ease
+                </li>
+                <li>
+                  <b-icon icon="dash"></b-icon>Remote live classes
+                </li>
+              </ul>
+            </div>
+          </div>
+        </b-col>
 
-    </section>
+        <!-- RIGHT PANE -->
+        <b-col sm="12" md="7" offset-md="5" class="p-0">
+          <div class="right-pane d-flex flex-column justify-content-start align-items-center">
+            <div class="right-pane-content">
+              <!-- BRAND MOCKUP -->
+              <div class="brand-mockup">
+                <img
+                  src="https://res.cloudinary.com/richy-jones/image/upload/v1587388460/GRADELY_ASSETS/GradelyMockup.png"
+                  alt="Gradely.Ng"
+                >
+              </div>
+              <!-- SECTION ONE -->
+              <div class="section-one">
+                <h3>Create your School Account</h3>
+              </div>
+
+              <!-- SECTION TWO -->
+              <div class="section-two">
+                <h4>
+                  Already have an account?
+                  <a href class="login-link orange-light">Login</a>
+                </h4>
+              </div>
+
+              <!-- REGISTRATION FORM -->
+              <SignUpForm/>
+              
+            </div>
+          </div>
+        </b-col>
+      </b-row>
+    </b-container>
+  </section>
+  <!-- SIGNUP PAGE  -->
 </template>
 
 <script>
-export default {
-    name: 'Signup'
+import SignUpForm from "@/components/signup/SignUpForm";
 
-}
+export default {
+  name: "Signup",
+  components: {
+    SignUpForm
+  }
+};
 </script>
 
 <style lang="scss">
-
 </style>
