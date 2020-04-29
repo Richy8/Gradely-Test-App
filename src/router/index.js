66e0ee38dec5251337873497829a771656cbba59
richy8 <menaelvisjones@gmail.com>
2020-04-29 12:19:19 +0100

    Update

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 
+import Home from "@/pages/Home"
 import schoolRoutes from "./schoolRoutes.js";
 
 Vue.use(VueRouter);
@@ -10,6 +11,7 @@ const routes = [
   {
     path: "/",
     name: "Home",
+    component: Home,
     meta: {
       title: 'Welcome to Gradely.Ng'
     }
